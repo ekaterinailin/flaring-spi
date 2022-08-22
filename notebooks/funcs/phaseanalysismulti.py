64705d43a49dde9f72128b418bd06235a736c5c1
Ekaterina Ilin <eilin@aip.de>
2022-08-22 11:49:46 +0200

    added a test to the calculate observing time func

--- notebooks/funcs/phaseanalysismulti.py
+++ notebooks/funcs/phaseanalysismulti.py
@@ -1,5 +1,51 @@
+import glob
+
+
 import pandas as pd 
 import numpy as np
+
+from astropy.io import fits
+
+
+import glob
+def read_lightcurves_from_series(series, location):
+    """
+    Read lightcurves using the information in the pandas.Series.
+
+    Parameters
+    ----------
+    series : pandas.Series
+        Dataframe containing the lightcurve information.
+    location : str
+        Location of the lightcurves ending with a slash.
+
+    Returns
+    -------
+    list of lightcurves
+    """
+
+    # read in the LC: get ID to look for in path
+    if series.mission == "Kepler":
+        ID = series.ID
+    elif series.mission == "TESS":
+        ID = series.TIC
+
+    # read in the LCs
+    try:
+        # find all paths that contain the following string
+        string = f"{series.timestamp}_{ID}_{series.quarter_or_sector:02}_altai_"
+      
+        paths = glob.glob(f"{location}{string}*")
+       
+        lcs = [fits.open(path)[1].data for path in paths]
+        if len(lcs) > 0:
+            return lcs
+        else:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        string = f"{ID}_{series.quarter_or_sector:02}_altai_0"
+       
+        return [fits.open(f"{location}{series.timestamp}_{string}.fits")[1].data]
 
 def get_cumulative_distribution(flare_table_single_star, observedphases, lcs):
     """Calculate the cumulative distribution function of observed
@@ -42,25 +88,29 @@ def get_cumulative_distribution(flare_table_single_star, observedphases, lcs):
         # Get the observed phases for this LC, or if it does not exist,
         # throw an error
         try:
-            obstimes_k = observedphases[lcid]
+            allcols = observedphases.columns.values
+            cols = allcols[np.isin(allcols, lcid)]
+            obstimes_k = observedphases[cols]
+
+            if len(cols) == 0:
+                raise ValueError(f"{lcid} column not found in observedphases table.")
         except KeyError:
+            print("no LC")
             raise KeyError(f"{lcid} column not found in observedphases table.")
 
-        print(lcid)
-
         # Check that flare phases are correct:
-        assert (obstimes_k <= 1).all(), \
+        assert (obstimes_k.values <= 1).all(), \
                f"One or more observedphases[{lcid}] > 1!"
-        assert (obstimes_k >= 0).all(), \
+        assert (obstimes_k.values >= 0).all(), \
                f"One or more observedphases[{lcid}] < 0!"
 
         # Get the total number of obstimes in this LC
-        tot_obstimes_k = obstimes_k.sum()
+        tot_obstimes_k = obstimes_k.sum().sum()
         pick_lc = ((flare_table_single_star.quarter_or_sector == row.quarter_or_sector) &
                    (flare_table_single_star.mission == row.mission))
         F_k = flare_table_single_star[pick_lc].shape[0]
-        print(F_k, tot_obstimes_k)
-        n_exp_k = (obstimes_k * F_k / tot_obstimes_k).values
+      
+        n_exp_k = (obstimes_k * F_k / tot_obstimes_k).sum(axis=1)
         n_exp += n_exp_k
         
     # for debugging: 
@@ -69,12 +119,76 @@ def get_cumulative_distribution(flare_table_single_star, observedphases, lcs):
     # cum_n_exp_alt = dfphases.sum(axis=1).values.cumsum() / dfphases.sum(axis=1).values.sum()
     
     # Calculate cumulative frequency distribution
-    _ =  n_exp.cumsum()
+    _ =  n_exp.cumsum().values
     
     # CDF maximum should be 1 
     cum_n_exp = _ / np.nanmax(_)
 
     # CDF minimum should be 0
+    print("cumnexp", cum_n_exp)
     cum_n_exp = np.insert(cum_n_exp, 0, 0)
     
     return n_exp, cum_n_exp#, cum_n_exp_alt return alternative only if necessary
+
+
+
+def get_observed_phases(p, lcs, location):
+    """Calculate the observed phases for a given set of flares.
+    
+    Parameters:
+    -----------
+    p : array
+        array of phases for the flares
+    lcs : pd.DataFrame
+        table that lists the target's LCs that were searched for flares
+    location : str
+        path to the folder with the LCs
+
+    Returns:
+    --------
+    observedphases : pd.DataFrame
+        table that lists the observed phases, with a column for each LC
+    binmids : array
+        array of bin midpoints
+    """
+    # bin array is two elements longer than the number of bins
+    # to include 0 and 1
+    bins = np.zeros(len(p) + 2) 
+
+    # add zero and one
+    bins[0] = 0 
+    bins[-1] = 1 
+
+    # and the others are kept as defined by observations
+    bins[1:-1] = p 
+
+    # define the bin mids
+    binmids = (bins[1:] + bins[:-1]) / 2
+
+    # initialize the observed phases table
+    observedphases = pd.DataFrame()
+
+    # loop over the LCs
+    for i, row in lcs.iterrows():
+   
+        # read in the lightcurve
+        lightcurves = read_lightcurves_from_series(row, location)
+       
+        for i, lightcurve in enumerate(lightcurves):
+         
+            # get the phases and bin them
+            counts, bins = np.histogram(lightcurve["phase"], bins=bins)
+
+            # circular boundary condition
+            counts[0] = counts[0] + counts[-1]
+
+            # remove last bin to avoid double counting
+            counts = counts[:-1]
+
+            # get observing times for each lightcurve using cadence
+            cadence = np.nanmin(np.diff(lightcurve["time"]))
+      
+            observedphases[f"{row.mission}_{row.quarter_or_sector}_{i}"] = counts * cadence
+
+    return observedphases, binmids
+
--- notebooks/funcs/tests/test_phaseanalysismulti.py
+++ notebooks/funcs/tests/test_phaseanalysismulti.py
@@ -1,8 +1,76 @@
+import os
+
 import pytest
 import pandas as pd
 import numpy as np
 
-from ..phaseanalysismulti import get_cumulative_distribution
+from astropy.io import fits
+from astropy.table import Table
+
+from ..phaseanalysismulti import (get_cumulative_distribution,
+                                  get_observed_phases,
+                                  )
+
+
+def _make_lightcurve(length, duration, period, phaseshift=0., 
+                     mission="Kepler", ID="123",TIC="456",qcs=1, i=0,
+                     tstamp = "2019-01-01", save=False, location="./"):
+    """Make synthetic light curve and save to file.
+
+    Parameters:
+    -----------
+    length : int
+        length of the light curve
+    duration : float
+        duration of the light curve in some unit
+    period : float
+        orbital period of the target in some unit
+    phaseshift : float
+        shift the phases by this amount if you want
+    save : bool
+        save the light curve to file
+    path : str
+        path to the folder where the light curve is saved
+
+    Returns:
+    --------
+    path to the file containing the light curve
+    """
+    # make a light curve phases
+    time = np.linspace(0, duration, length)
+    print(time[-3:], time[:3])
+    phases = (time % period) / period
+
+    # make a light curve with a phase shift
+    phases_shift = (phases + phaseshift) % 1
+   
+    # create pd.Series with information about the LC
+    lc = pd.Series({"mission": mission, 
+                    "ID": ID, 
+                    "TIC": TIC,
+                    "quarter_or_sector": qcs,
+                    "timestamp": tstamp, 
+                    "duration": duration})
+
+    if mission == "Kepler":
+        hostname = ID
+    elif mission == "TESS":
+        hostname = TIC
+
+    # create a path to the file using the information about the LC
+    path = f"{location}{tstamp}_{hostname}_{qcs:02}_altai_{i}.fits"
+
+    # save the light curve to a fits file
+    if save:
+        # make a fits file
+        hdu = fits.PrimaryHDU(np.array([1.,2.,3.]))
+        data = fits.BinTableHDU(data=Table.from_pandas(pd.DataFrame({"time":time, 
+                                   "phase":phases_shift})))
+        hdulist = fits.HDUList([hdu, data])
+        hdulist.writeto(path, overwrite=True)
+
+    return path, lc
+    
 
 def test_get_cumulative_distribution():
     """Test the get_cumulative_distribution function.
@@ -107,7 +175,47 @@ def test_get_cumulative_distribution():
         assert "One or more observedphases" in str(e.value)
 
     # Test 2: LC is missing in the table of observed phases
-    with pytest.raises(KeyError) as e:
+    with pytest.raises(ValueError) as e:
         lcs["mission"] = ["TESS"] * 3
         get_cumulative_distribution(flares, observedphases, lcs)
         assert "column not found in observedphases" in str(e.value)
+
+
+def test_get_observed_phases():
+    """Test the get_observed_phases function. Integration test only so far"""
+
+    # create three light curves
+    path1, lc1 = _make_lightcurve(length=10000, duration=10., period=1.,
+                                phaseshift=0, save=True, location="./")
+    path2, lc2 = _make_lightcurve(length=20000, duration=10., period=1., i=1,
+                                phaseshift=0., save=True, location="./")
+    path3, lc3 = _make_lightcurve(length=20000, duration=10., period=1., i=0,
+                                mission="TESS",
+                                phaseshift=0., save=True, location="./")
+
+    # concatenate lc1 and lc2 to a DataFrame
+    lcs = pd.concat([lc1, lc2, lc3], axis=1).T
+
+    # create an array of flare phases
+    phases = [0.5, 0.6, 0.7, .8]
+
+    # get the observed phases for the flares
+    observedphases, binmids = get_observed_phases(phases, lcs, "./")
+
+    # check that the observed phases are correct
+    assert observedphases.iloc[0,:].values == pytest.approx(7., rel=1e-3)
+    assert observedphases.iloc[1::,:].values == pytest.approx(1., rel=1e-3)
+
+    # total observing time should be correct
+    assert (observedphases.sum(axis=0).values == pytest.approx(10.,rel=1e-2))
+
+    # assert all columns are correctly read in
+    assert (observedphases.columns.values == 
+            ["Kepler_1_0", "Kepler_1_1", "TESS_1_0"]).all()
+
+    # assert the number of bins is correct
+    assert observedphases.shape[0] == len(phases)
+
+    # delete the file created
+    for p in [path1, path2, path3]:
+        os.remove(p)
