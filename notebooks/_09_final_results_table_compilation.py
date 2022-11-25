448bc6a6b12e9e7172b481e350097454c1c10d4e
Ekaterina Ilin <eilin@aip.de>
2022-11-25 14:54:34 +0100

    compiled results table, notebook for plots left

---
+++ notebooks/_09_final_results_table_compilation.py
@@ -0,0 +1,126 @@
+"""
+UTF-8, Python 3
+
+------------------
+Flaring SPI
+------------------
+
+Ekaterina Ilin, 2022, MIT License
+
+This script calculates a bunch of values about the AD tests and stellar systems,
+ including:
+
+- the magnetic field of the star based on X-ray luminosity (Reiners+2022)
+- the total observing time per star
+- number of covered orbits per star
+- relative velocity between star and planet at the planets orbit
+- SPI power (Lanya 2009 and Lanza 2012)
+
+Final table is called: results.csv
+
+* https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=PSCompPars
+** https://exoplanetarchive.ipac.caltech.edu/docs/API_PS_columns.html
+"""
+
+
+import numpy as np
+import pandas as pd
+
+from funcs.ad import aggregate_pvalues
+from funcs.spirelations import (b_from_lx_reiners,
+                                wrap_obstimes,
+                                p_spi_lanza12,
+                                calculate_relative_velocity)
+
+
+if __name__ == "__main__":
+
+    # -------------------------------------------------------------------------
+    # First, read in all the data you want to combine, and will need for the 
+    # calculations
+
+    # [READ] 
+    # in AD test results
+    adtests = pd.read_csv("../results/multistar_adtest.csv")
+
+    # [READ] 
+    # in the table of known parameters from the NASA Exoplanet Archive and TESS
+    sps_w_ad = pd.read_csv(
+        "../results/params_of_star_planet_systems_with_AD_tests.csv")
+
+    # rename column tic_id to TIC
+    sps_w_ad.rename(columns={"tic_id": "TIC"}, inplace=True)
+
+    # [READ] 
+    # in age and rotation period from the literature search
+    literature_params = pd.read_csv(
+        "../results/2022_08_stellar_params.csv")
+
+    # [READ] 
+    # in the flare table
+    flares = pd.read_csv("../results/2022_07_flares_vetted.csv")
+
+    # -------------------------------------------------------------------------
+    # Next, initialize the final table by aggregating the p-values for the AD tests
+
+    # aggregate the p-values
+    mean_std = aggregate_pvalues(adtests, subsample="ED>1s", period="orbit")
+
+    # Then merge in the NASA Exoplanet Archive table and the literature 
+    # search table
+
+    # merge the AD test results with the stellar parameters
+    mean_std = mean_std.merge(literature_params, on=["TIC"], how="left")
+
+    # add semi-major axis in AU, stellar radius in Rsun, 
+    # planet radius in Rjup, and orbital period to the table:
+
+    # merge teh relevant part of the table
+    mean_std = mean_std.merge(sps_w_ad[["TIC", "pl_orbsmax","st_rad_kepler",
+                                        "st_rad_tess", "pl_radj", "pl_orbper_kepler",
+                                        "pl_orbper_tess"]],
+                                on="TIC", how="left")
+
+    # rename columns and fill NaNs will TESS values
+    mean_std = mean_std.rename(columns = {"pl_orbsmax": "a_au",})
+    mean_std["st_rad"] =  mean_std.st_rad_kepler.fillna(mean_std.st_rad_tess)
+    mean_std["orbper_d"] =  mean_std.pl_orbper_kepler.fillna(mean_std.pl_orbper_tess)
+
+    # -------------------------------------------------------------------------
+    # Add some values manually
+
+    # HIP 67522
+    mean_std.loc[mean_std.TIC == "166527623", "a_au"] = 1.5
+
+
+    # -------------------------------------------------------------------------
+    # Now it's time to calculate some properties of the systems with
+    # AD tests:
+
+    # calculate B field from X-ray luminosity and stellar radius
+    mean_std["B_G"] = mean_std.apply(lambda x: b_from_lx_reiners(x.xray_flux_erg_s, x.st_rad ), axis=1)
+
+    # get the observing time for each star using the flare tables
+    mean_std["obstime_d"] = mean_std.apply(lambda x: wrap_obstimes(str(x.TIC), 
+                                                    flares), axis=1)
+
+    # calculate the number of covered orbits
+    mean_std["orbits_covered"] = mean_std["obstime_d"] / mean_std["orbper_d"]
+
+    # calculate relative velocity between stellar rotation at the planetary orbit
+    # and the orbital velocity of the planet in km/s
+    mean_std["v_rel_km_s"] = mean_std.apply(lambda x: 
+                                            calculate_relative_velocity(x.a_au,
+                                            x.orbper_d, x.st_rotp), axis=1)
+
+
+    # calculate the SPI power from the Lanza 2012 scaling relation
+    mean_std["p_spi_erg_s"] = mean_std.apply(lambda x: p_spi_lanza12(np.abs(x.v_rel_km_s),
+                                                            x.B_G, x.pl_radj), axis=1)   
+
+    # -------------------------------------------------------------------------
+    # Finally, save the table
+
+    # [WRITE] 
+    # to file
+    mean_std.to_csv("../results/results.csv", index=False)
