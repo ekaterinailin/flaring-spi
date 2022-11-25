9d7f3dd0c0a25ca341b967e0c7810778f6d33fc9
Ekaterina Ilin <eilin@aip.de>
2022-11-25 13:15:21 +0100

    further modularization of functions in the postprocessing stage

--- notebooks/funcs/spirelations.py
+++ notebooks/funcs/spirelations.py
@@ -2,6 +2,71 @@ import numpy as np
 import astropy.units as u
 from astropy.constants import R_jup, R_sun
 
+
+def convert_datapoints_to_obstime(n, mission):
+    """Calculate the observing time in days.
+    based on the number of datapoints in the LC.
+
+    Parameters
+    ----------
+    n : int
+        Number of datapoints.
+    mission : str
+        Mission name, either "Kepler" or "TESS". 
+
+    Returns
+    -------
+    obstime : float
+        Observing time in days.
+    """
+    if n > 50000:
+        if mission == "TESS":
+            return n / 3. / 60. / 24.  # 20 sec cadence
+        elif mission == "Kepler":
+            return n / 60. / 24.  # 1 min cadence
+        else:
+            raise ValueError("Mission name not recognized for a large LC.")
+    else:
+        if mission == "TESS":
+            return n * 2. / 60. / 24.  # 2 min cadence
+        elif mission == "Kepler":
+            return n / 60. / 24.  # 1 min cadence
+        else:
+            raise ValueError("Mission name not recognized for a small LC.")
+
+
+def wrap_obstimes(TIC, flares):
+    """Wrap the obstime function for use with multiple
+    LCs for a given TIC ID. NOT TESTED!
+
+    Parameters
+    ----------
+    TIC : str
+        TIC ID.
+    flares : pandas.DataFrame
+        Flare table with total_n_valid_data_points and mission columns.
+
+    Returns
+    -------
+    obstime_d: float
+        Total observing time in days.
+    """
+    # pick up all the rows with LCs searched for flares
+    sysvalues = flares[flares.TIC.astype(str) == TIC]
+
+    # drop the duplicates with multiple flares per LC
+    vals = sysvalues[["total_n_valid_data_points",
+                      "mission"]].drop_duplicates()
+  
+    # calculate the observing time based on the 
+    # number of data points in the LC
+    # and summing up the observing time for all LCs
+    obstime_d = vals.apply(lambda x: convert_datapoints_to_obstime(
+        x.total_n_valid_data_points, x.mission), axis=1).sum()
+
+    return obstime_d
+
+
 def p_spi_lanza12(v_rel, B, pl_rad, Bp=1.):
     """Lanza 2012 scaling relation
     
@@ -44,3 +109,30 @@ def b_from_lx_reiners(lx, r):
   
     B =  np.power(lx / 3.28e-12, 1. / 1.58) / (4. * np.pi * rcm**2) 
     return B
+
+
+def calculate_relative_velocity(a_au, orbper, rotper):
+    """Calculate the relative velocity between stellar rotation at the planetary orbit
+    and the orbital distance of the planet in km/s.
+
+    Parameters
+    ----------
+    au : float
+        Semi-major axis in AU.
+    orbper : float
+        Orbital period in days.
+    rotper : float
+        Stellar rotation period in days.
+
+    Returns
+    -------
+    v_rel_km_s : float
+        Relative velocity between stellar rotation at the planetary orbit
+        and the orbital velocity of the planet in km/s.
+    """
+    v_rel = 2 * np.pi * a_au * (1/orbper - 1/rotper) 
+    
+    # return in km/s
+    # 1AU = 149597870.700 km
+    # 1day = 24h * 3600s
+    return v_rel / 24. / 3600. * 149597870.700
--- notebooks/funcs/tests/test_spirelations.py
+++ notebooks/funcs/tests/test_spirelations.py
@@ -1,8 +1,36 @@
+import pytest
 import numpy as np
 from astropy.constants import R_jup, R_sun
 
-from ..spirelations import (p_spi_lanza12,
-                            b_from_lx_reiners)
+
+from ..spirelations import (convert_datapoints_to_obstime,
+                            p_spi_lanza12,
+                            b_from_lx_reiners,
+                            calculate_relative_velocity)
+
+
+
+def test_convert_datapoints_to_obstime():
+    """Test that conversion works as expected,
+    and that error are thrown at the right places."""
+
+
+    # 1 TESS data point is two minute
+    assert convert_datapoints_to_obstime(1, "TESS") * 24. * 60. == 2. 
+
+    # 1 Kepler data point is one minute
+    assert convert_datapoints_to_obstime(1, "Kepler") * 24. * 60. == 1.
+
+    # 1 TESS data point is 20 seconds
+    assert convert_datapoints_to_obstime(1e5, "TESS") * 24. * 60. * 60. == pytest.approx(20. * 1e5)
+
+
+    with pytest.raises(ValueError):
+        convert_datapoints_to_obstime(1, "Chandra")
+        
+    with pytest.raises(ValueError):
+        convert_datapoints_to_obstime(1e5, "Chandra")
+
 
 def test_p_spi_lanza():
     """Tiny test for SPI power function"""
@@ -17,7 +45,20 @@ def test_b_from_lx_reiners():
     """Tiny test for Reiners et al. 2022 Eq. 4"""
 
     # input unit conversion check
-    (3.28e-12, 1. / (R_sun.to("cm").value)) * 4. * np.pi == 1.
+    assert b_from_lx_reiners(3.28e-12, 1. / (R_sun.to("cm").value)) * 4. * np.pi == 1.
 
     # input NaN returns NaN
     np.isnan(b_from_lx_reiners(3.28e-12, np.nan))
+
+
+def test_calculate_relative_velocity():
+    "Test if the formula converts properly."
+    # synchronized orbit
+    assert calculate_relative_velocity(1, 1, 1) == 0
+
+    # no orbital distance, planet is in the star
+    assert calculate_relative_velocity(0, 1, 3) == 0.
+
+    # unit check
+    assert (calculate_relative_velocity(1 /2 / np.pi, 1, 2) / 
+            149597870.700 * 24. * 3600. == 0.5)
